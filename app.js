13ce4a8785549c8344395962662af8d5ea041e5b
unknown <corentin.malbrun1@gmail.com>
2022-05-04 14:20:18 +0200

    portfolio1.0

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-let img__slider = document.getElementsByClassName('img__slider');
+var app = document.getElementById('app');
 
-let etape = 0;
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 150,
+});
 
-let nbr__img = img__slider.length;
-
-let precedent = document.querySelector('.precedent');
-let suivant = document.querySelector('.suivant');
-
-function enleverActiveImages() {
-    for(let i = 0; i < nbr__img ; i++) {
-        img__slider[i].classList.remove('active');
-    }
-}
-
-suivant.addEventListener('click', function() {
-    etape++;
-    if(etape >= nbr__img){
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
-
-precedent.addEventListener('click', function(){
-    etape--;
-    if(etape < 0){
-        etape = nbr__img -1;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
-
-setInterval(function(){
-    etape++;
-    if(etape >= nbr__img){
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-},6500)
+typewriter
+  .pauseFor(1000)
+  .typeString('Développeur web')
+  .pauseFor(1500)
+  .deleteChars(3)
+  .typeString('Full stack ')
+  .start();
