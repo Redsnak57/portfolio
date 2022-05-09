3ad537fb9b9ea181f146cca405998edaa4061076
unknown <corentin.malbrun1@gmail.com>
2022-05-09 15:58:56 +0200

    mediaquery

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
   loop: false,
-  delay: 150,
+  delay: 100,
 });
 
 typewriter
   .pauseFor(1000)
   .typeString('Développeur web')
-  .pauseFor(1500)
+  .pauseFor(1000)
   .deleteChars(3)
   .typeString('Full stack ')
+  .pauseFor(1000)
   .start();
+
